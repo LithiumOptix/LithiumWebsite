638f91c01b4e6337a21fa966cb1322430ff0bd82
LithiumOptix <hbank3664@gmail.com>
2026-04-07 11:55:12 -0600

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import path from "path" // You might need to install 'path' or '@types/node'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
